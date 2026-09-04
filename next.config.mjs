e338c3c4dc34f73cca0ace884d15e3a61dfa2b2b
Ashutosh Gola <ashutoshgola91@gmail.com>
2026-09-05 03:01:26 +0530

    Redirect root URL to /products

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/health',
+        destination: '/api/health',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
